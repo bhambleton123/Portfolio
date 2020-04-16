b433d05d189fa546e06cc888b2949c15907367ae
Brian Hambleton <bhambl3t0n1@gmail.com>
2020-04-15 17:44:17 -0700

    Added validation for mail form on portfolio, changed formatting for mail sender api

--- client/src/home/contact/email-form.js
+++ client/src/home/contact/email-form.js
@@ -1,36 +1,70 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   FormControl,
-  Input,
-  InputLabel,
   Button,
   Box,
+  TextField,
   TextareaAutosize,
 } from "@material-ui/core";
 
-
 export default function EmailForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [company, setCompany] = useState("");
+  const [message, setMessage] = useState("");
+
+  const preventSubmitOnEnter = (keyEvent) => {
+    if ((keyEvent.charCode || keyEvent.keyCode) === 13) {
+      keyEvent.preventDefault();
+    }
+  };
+
+  const submitEmail = (e) => {
+    e.preventDefault();
+    setName("");
+    setEmail("");
+    setCompany("");
+    setMessage("");
+  };
+
   return (
-    <Box display="flex" justifyContent="center" flexDirection="column">
-      <FormControl>
-        <InputLabel>Name</InputLabel>
-        <Input></Input>
-      </FormControl>
-      <FormControl>
-        <InputLabel>Email</InputLabel>
-        <Input type="email"></Input>
-      </FormControl>
-      <FormControl>
-        <InputLabel>Company (optional)</InputLabel>
-        <Input type="email"></Input>
-      </FormControl>
-      <FormControl margin="dense">
-        <TextareaAutosize
-          rows={5}
-          placeholder="Your message"
-        ></TextareaAutosize>
-      </FormControl>
-      <Button>Send</Button>
-    </Box>
+    <form onSubmit={submitEmail} onKeyDown={preventSubmitOnEnter}>
+      <Box display="flex" justifyContent="center" flexDirection="column">
+        <FormControl>
+          <TextField
+            required
+            label="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </FormControl>
+        <FormControl>
+          <TextField
+            required
+            label="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormControl>
+        <FormControl>
+          <TextField
+            label="Company (optional)"
+            value={company}
+            onChange={(e) => setCompany(e.target.value)}
+          />
+        </FormControl>
+        <FormControl margin="dense">
+          <TextareaAutosize
+            required
+            rows={5}
+            placeholder="Your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></TextareaAutosize>
+        </FormControl>
+        <Button type="submit">Send</Button>
+      </Box>
+    </form>
   );
 }
--- server/controllers/mailer.js
+++ server/controllers/mailer.js
@@ -10,11 +10,29 @@ const mailToBrian = (req, res) => {
     },
   });
 
+  const createContent = (name, email, message, company) => {
+    return {
+      title: company
+        ? `${name} from ${company} sent you a message`
+        : `${name} sent you a message`,
+      html: `<p>${message}</p>
+        <br/>
+        <p>Email: ${email}</p>`,
+    };
+  };
+
+  const content = createContent(
+    req.body.name,
+    req.body.email,
+    req.body.message,
+    req.body.company
+  );
+
   const mailOptions = {
     from: process.env.MAILER_USERNAME,
     to: "bhambl3t0n1@gmail.com",
-    subject: req.body.subject,
-    html: req.body.html,
+    subject: content.title,
+    html: content.html,
   };
 
   transporter.sendMail(mailOptions, (err, info) => {
