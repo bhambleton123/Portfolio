689d14a1c16f7d7563a4ddb83020f743bae0e638
Brian Hambleton <bhambl3t0n1@gmail.com>
2020-04-15 18:11:37 -0700

    Added async alerts to display to the user when email is sending, when email is sent, and if there is an error

--- client/src/home/contact/email-form.js
+++ client/src/home/contact/email-form.js
@@ -6,12 +6,18 @@ import {
   TextField,
   TextareaAutosize,
 } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+import AlertTitle from "@material-ui/lab/AlertTitle";
+import axios from "axios";
 
 export default function EmailForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [company, setCompany] = useState("");
   const [message, setMessage] = useState("");
+  const [sendSentAlert, setSendSentAlert] = useState(false);
+  const [sendSendingAlert, setSendSendingAlert] = useState(false);
+  const [sendErrorAlert, setSendErrorAlert] = useState(false);
 
   const preventSubmitOnEnter = (keyEvent) => {
     if ((keyEvent.charCode || keyEvent.keyCode) === 13) {
@@ -21,15 +27,69 @@ export default function EmailForm() {
 
   const submitEmail = (e) => {
     e.preventDefault();
-    setName("");
-    setEmail("");
-    setCompany("");
-    setMessage("");
+
+    setSendSendingAlert(true);
+    axios
+      .post("/api/mail", {
+        name,
+        email,
+        company,
+        message,
+      })
+      .then((res) => {
+        setName("");
+        setEmail("");
+        setCompany("");
+        setMessage("");
+        setSendSendingAlert(false);
+        setSendSentAlert(true);
+        setTimeout(() => {
+          setSendSendingAlert(false);
+        }, 5000);
+      })
+      .catch((err) => {
+        setSendErrorAlert(true);
+        setTimeout(() => {
+          setSendErrorAlert(false);
+        }, 5000);
+        console.error(err);
+      });
   };
 
   return (
     <form onSubmit={submitEmail} onKeyDown={preventSubmitOnEnter}>
-      <Box display="flex" justifyContent="center" flexDirection="column">
+      <Box
+        display="flex"
+        justifyContent="center"
+        flexDirection="column"
+        maxWidth="250px"
+        minWidth="200px"
+      >
+        {sendSentAlert ? (
+          <Alert severity="success">
+            <AlertTitle>Success!</AlertTitle>
+            Your email was successfully sent! I'll get back to you soon at the
+            email provided.
+          </Alert>
+        ) : (
+          ""
+        )}
+        {sendSendingAlert ? (
+          <Alert severity="info">
+            <AlertTitle>Sending email...</AlertTitle>
+            Currently working on sending your email to Brian.
+          </Alert>
+        ) : (
+          ""
+        )}
+        {sendErrorAlert ? (
+          <Alert severity="error">
+            <AlertTitle>Error</AlertTitle>
+            Email was not successfully sent.
+          </Alert>
+        ) : (
+          ""
+        )}
         <FormControl>
           <TextField
             required
